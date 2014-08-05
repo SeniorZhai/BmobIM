60a173730460ef102aff54e3add86a0bbdaea71f
仔仔 <370985116@qq.com>
2014-08-05 16:35:47 +0800

    first commit

---
+++ src/com/bmob/im/demo/config/Config.java
@@ -0,0 +1,12 @@
+package com.bmob.im.demo.config;
+
+/** 系统变量
+  * @ClassName: Config
+  * @Description: TODO
+  * @author smile
+  * @date 2014-6-17 上午9:40:11
+  */
+public class Config {
+
+	public static String applicationId = "bc99c4ebc1c285a9e394becb08c3a72c";
+}
